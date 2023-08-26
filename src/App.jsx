9e71bae1c2efef63fc4967130807a30f354e0bce
Noor <noorulainmalik980@gamil.com>
2023-08-26 17:56:53 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Navigation from "./component/navigation";
+import HeroSection from "./component/Hero";
+const App=() => {
+
+
+    return<div>
+     <Navigation/>
+     <HeroSection/>
+    </div>;
+
+
+};
+
+
+
+
+export default App;
